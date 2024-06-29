cb24f65e24ce7652beec11b7dd090baf0f5d2ea4
azmi adan <azmi.adan2032@gmail.com>
2024-06-29 10:21:10 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31
+const num2 =2
+const multiply = num1*num2
+const random = Math.floor(Math.random()*100)+1
+const num3 =25
+const num4 =7
+const mod = num3%num4
+const setOfNumbers = [10, 5, 20, 15, 8];
+const max = Math.max(...setOfNumbers);
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
